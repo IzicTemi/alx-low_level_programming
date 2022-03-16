435fce0da7f741f6aa95ff4a1959b85afb3f3fd1
Newton2541 <iomolayo2541@gmail.com>
2022-03-16 02:33:42 +0100

    Chnages

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,11 @@
-#include "holberton.h"
-
 /**
  * Print _putchar, followed by an new line
  *
  * Return: On success 0.
  * On error, 1 is returned, and errno is set appropriately.
  */
-void _putchar(void)
+int main (void)
 {
-	char str1[12] = "_putchar";
-	printf("%s\n", str1);
-
-	return 0;
+    puts("_putchar");
+    return 0;
 }
